b0cf71087cdb89dc62444bb05a94c405dac7e2af
Hugo Rayhan Firmansyah <hugo.hgrayhan08@gmail.com>
2023-05-28 17:59:38 +0700

    Upload file pertama todo list app

---
+++ lib/style/color_style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+Color primary1 = const Color(0xffE7F6F2);
+Color primary2 = const Color(0xffA5C9CA);
+Color primary3 = const Color(0xff557B83);
+Color primary4 = const Color(0xff395B64);
+Color btnAdd = const Color(0xff68D051);
